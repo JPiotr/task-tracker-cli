5380fe4b8a4bb29dda84202ab1eccc2ccd95e766
Oblitus <piotr5jurczak@gmail.com>
2024-12-01 12:13:57 +0100

    delete unused import

--- src/main/java/com/oblitus/routemap/sh/projects/core/TaskMenager.java
+++ src/main/java/com/oblitus/routemap/sh/projects/core/TaskMenager.java
@@ -1,7 +1,6 @@
 package com.oblitus.routemap.sh.projects.core;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
